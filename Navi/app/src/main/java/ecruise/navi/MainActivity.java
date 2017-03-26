389472d508e7bd076fe0833abb7284f62d4c60c9
ivasylie <rodiy@ukr.net>
2017-03-26 11:48:05 +0200

    Testing commit. Added new GMap Item

--- Navi/app/src/main/java/ecruise/navi/MainActivity.java
+++ Navi/app/src/main/java/ecruise/navi/MainActivity.java
@@ -1,7 +1,10 @@
 package ecruise.navi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -11,5 +14,13 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*Button mLoginButton = (Button) findViewById(R.id.centerButton);
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(intent);
+            }
+        });*/
     }
 }
